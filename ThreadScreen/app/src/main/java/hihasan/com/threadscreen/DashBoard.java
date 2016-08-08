a5c2bc142954dbd9605d74d1f6413697fd798e00
hihsan <hasan@hihasan.com>
2016-08-08 18:25:24 +0600

    Second Screen
    Add Screen. Now after 5 second first layout  navigate to second layout

---
+++ ThreadScreen/app/src/main/java/hihasan/com/threadscreen/DashBoard.java
@@ -0,0 +1,18 @@
+package hihasan.com.threadscreen;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mamun on 8/8/2016.
+ */
+public class DashBoard extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        //setContectView()
+        setContentView(R.layout.dashboard);
+    }
+}
--- ThreadScreen/app/src/main/java/hihasan/com/threadscreen/Startup.java
+++ ThreadScreen/app/src/main/java/hihasan/com/threadscreen/Startup.java
@@ -1,37 +1,32 @@
 package hihasan.com.threadscreen;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
 
-public class Startup extends AppCompatActivity {
+public class Startup extends Activity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        //setContectView()
         setContentView(R.layout.activity_startup);
+
+        //Thread Handler for 5 sec.
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent mainIntent = new Intent(Startup.this, DashBoard.class);
+                Startup.this.startActivity(mainIntent);
+                Startup.this.finish();
+            }
+        }, 5000);
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_startup, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
