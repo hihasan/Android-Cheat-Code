42543eeb2e89aa99a4123f53eb6d55122886d77e
hihsan <hasan@hihasan.com>
2016-08-08 19:56:41 +0600

    Update 1
    Button Added to the Mainscreen And onClick Method implemented. Also
    Toast message is included............

--- ButtonAction/app/src/main/java/hihasan/com/buttonaction/MainActivity.java
+++ ButtonAction/app/src/main/java/hihasan/com/buttonaction/MainActivity.java
@@ -1,16 +1,31 @@
 package hihasan.com.buttonaction;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private Button button;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View arg0)
+            {
+                Toast.makeText(getApplicationContext(), "Button is clicked", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
