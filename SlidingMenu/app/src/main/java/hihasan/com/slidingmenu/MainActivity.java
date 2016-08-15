985d1992e9b825630f71f0c4cdc1fedda15dddb3
hihsan <hasan@hihasan.com>
2016-08-16 02:36:37 +0600

    Sliding Menu
    Sliding Menu Screen

---
+++ SlidingMenu/app/src/main/java/hihasan/com/slidingmenu/MainActivity.java
@@ -0,0 +1,57 @@
+package hihasan.com.slidingmenu;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity
+{
+    String[] menu;
+    DrawerLayout dLayout;
+    ListView dList;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        menu = new String[]{"Dashboard","Profile","Windows","Team","List Of Info","About Us","Logout","Exit"};
+        dLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        dList = (ListView) findViewById(R.id.left_drawer);
+
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,menu);
+
+        dList.setAdapter(adapter);
+        dList.setSelector(android.R.color.holo_blue_dark);
+
+        dList.setOnItemClickListener(new OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View v, int position, long id) {
+
+                dLayout.closeDrawers();
+                Bundle args = new Bundle();
+                args.putString("Menu", menu[position]);
+                Fragment detail = new DetailFragment();
+                detail.setArguments(args);
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.content_frame, detail).commit();
+
+            }
+
+        });
+    }
+
+
+
+}
