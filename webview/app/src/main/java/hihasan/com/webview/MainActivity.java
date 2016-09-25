35b9d712d8e90b03b3562ffbd2ed0c32e67509da
hihsan <hasan@hihasan.com>
2016-09-26 02:03:14 +0600

    add url
    add custom url

--- webview/app/src/main/java/hihasan/com/webview/MainActivity.java
+++ webview/app/src/main/java/hihasan/com/webview/MainActivity.java
@@ -2,9 +2,12 @@ package hihasan.com.webview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity
 {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -12,5 +15,22 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new MyWebViewClient());
+
+        String url = "http://hihasan.com";
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+
+    }
+
+    private class MyWebViewClient extends WebViewClient
+    {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url)
+        {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
