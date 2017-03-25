6e510aa07706e4a7d0a0b139afbc39b0374b3edc
hihsan <hasan@hihasan.com>
2017-03-25 15:45:23 +0600

    Java Code

--- AndroidWifi/app/src/main/java/com/hihasan/androidwifi/MainActivity.java
+++ AndroidWifi/app/src/main/java/com/hihasan/androidwifi/MainActivity.java
@@ -1,13 +1,46 @@
 package com.hihasan.androidwifi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private Button enableWIfi, diableWifi;
+    private WifiManager wifi;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enableWIfi=(Button) findViewById (R.id.button1);
+        diableWifi=(Button) findViewById (R.id.button2);
+
+        button();
+    }
+
+    private void button()
+    {
+        enableWIfi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                wifi=(WifiManager) getSystemService(Context.WIFI_SERVICE);
+                wifi.setWifiEnabled(true);
+
+            }
+        });
+
+        diableWifi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                wifi=(WifiManager) getSystemService (Context.WIFI_SERVICE);
+                wifi.setWifiEnabled(false);
+            }
+        });
     }
 }
