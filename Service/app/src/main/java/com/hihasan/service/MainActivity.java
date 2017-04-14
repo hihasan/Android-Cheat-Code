9d5415bffb9a748a2aec31ac48bdec31c473ab5c
hihsan <hasan@hihasan.com>
2017-04-14 16:34:59 +0600

    Mainactivity Button Method Initialize

--- Service/app/src/main/java/com/hihasan/service/MainActivity.java
+++ Service/app/src/main/java/com/hihasan/service/MainActivity.java
@@ -2,12 +2,40 @@ package com.hihasan.service;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private AppCompatButton b1,b2;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1=(AppCompatButton) findViewById(R.id.start);
+        b2=(AppCompatButton) findViewById(R.id.cancel);
+
+        //on Button Press Method
+        toButton();
+    }
+
+    private void toButton()
+    {
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
