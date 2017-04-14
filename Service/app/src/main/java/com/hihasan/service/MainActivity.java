5f4d94c3e1e02c695c171d7dc202654b47038a97
hihsan <hasan@hihasan.com>
2017-04-14 17:39:55 +0600

    onClick Method

--- Service/app/src/main/java/com/hihasan/service/MainActivity.java
+++ Service/app/src/main/java/com/hihasan/service/MainActivity.java
@@ -1,13 +1,22 @@
 package com.hihasan.service;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import java.util.Calendar;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
     private AppCompatButton b1,b2;
+    private PendingIntent pedingIntent;
+    private AlarmManager alarmManager;
+    public static int i=10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,7 +35,16 @@ public class MainActivity extends AppCompatActivity
     {
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
+                Intent myIntent=new Intent(MainActivity.this,BackgroundService.class);
+                pedingIntent=PendingIntent.getService(MainActivity.this,0,myIntent,0);
+                alarmManager=(AlarmManager) getSystemService(ALARM_SERVICE);
+                Calendar calender= Calendar.getInstance();
+                calender.setTimeInMillis(System.currentTimeMillis());
+                calender.add(Calendar.SECOND,10);
+                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+(i*1000),(i*1000),pedingIntent));
+                Toast.makeText(getApplicationContext(),"Alarm Start",Toast.LENGTH_SHORT).show();
 
             }
         });
