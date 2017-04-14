8f8ceebf5f94a3c8a764a536b1b7dc071bd18210
hihsan <hasan@hihasan.com>
2017-04-14 16:38:56 +0600

    Create Service

---
+++ Service/app/src/main/java/com/hihasan/service/BackgroundService.java
@@ -0,0 +1,21 @@
+package com.hihasan.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+/**
+ * Created by Mamun on 4/14/2017.
+ */
+
+public class BackgroundService extends Service
+{
+
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        Toast.makeText(this,"onBind Method",Toast.LENGTH_SHORT).show();
+        return null;
+    }
+}
--- Service/app/src/main/java/com/hihasan/service/MainActivity.java
+++ Service/app/src/main/java/com/hihasan/service/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
             }
         });
     }
